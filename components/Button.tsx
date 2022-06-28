01db60cadfebe556c22769724194f43b0bb7df3b
Joan Gavelán <joangavelan@gmail.com>
2022-06-28 14:50:23 -0500

    build: implement sass

---
+++ components/Button.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../styles/Button.module.scss'
+
+const Button = () => {
+  return (
+    <button className={styles.button}>Button</button>
+  )
+}
+
+export default Button
