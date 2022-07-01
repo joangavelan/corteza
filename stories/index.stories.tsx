a8ccefe1735424636645310e0b059d16e98ee78d
Joan Gavelán <joangavelan@gmail.com>
2022-07-01 17:02:41 -0500

    chore: remove index.stories.tsx file

--- stories/index.stories.tsx
+++
@@ -1,9 +0,0 @@
-import ButtonComp from '../components/Button'
-import Home from '../pages/index'
-
-export default {
-  title: 'UI'
-}
-
-export const Button = () => <ButtonComp />
-export const HomePage = () => <Home />
