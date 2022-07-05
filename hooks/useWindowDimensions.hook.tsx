bf5b646f42251cc6cdf0365986b91b72e57df3a0
Joan Gavelán <joangavelan@gmail.com>
2022-07-05 14:57:54 -0500

    feat: create useWindowsDimensions custom hook

---
+++ hooks/useWindowDimensions.hook.tsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+import { getWindowDimensions } from "@utils";
+
+export function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowDimensions;
+}
