8c7c2486a90f7d20ed7ac532516c510acc0b1113
Joan Gavelán <joangavelan@gmail.com>
2022-07-15 12:37:43 -0500

    feat: implement global state functionality for search query

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import styles from '../styles/SearchBar.module.scss'
+import styles from '@styles/SearchBar.module.scss'
+import useStore from '@zustand/store'
 
-const SearchBar: React.FC = () => {
+const SearchBar = () => {
+  const searchQuery = useStore((state) => state.searchQuery)
+  const setSearchQuery = useStore((state) => state.setSearchQuery)
+
   return (
     <input
       className={styles.searchBar}
-      placeholder='e.g: Blue Economy'
+      placeholder='e.g: Blue Economy (Gunter Pauli)'
       name='search'
       type='text'
       aria-label='search'
+      value={searchQuery}
+      onChange={(e) => setSearchQuery(e.target.value)}
     />
   )
 }
