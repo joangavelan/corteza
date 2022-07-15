a71d8272c477bdf84d4e86bc74a523e5b9cb8a24
Joan Gavelán <joangavelan@gmail.com>
2022-07-15 12:23:47 -0500

    refactor: remove args, import comp with alias

--- stories/SearchBar.stories.tsx
+++ stories/SearchBar.stories.tsx
@@ -1,14 +1,12 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react'
-import SearchBar from '../components/SearchBar'
+import SearchBar from '@components/SearchBar'
 
 export default {
   title: 'UI/SearchBar',
   component: SearchBar
 } as ComponentMeta<typeof SearchBar>
 
-const Template: ComponentStory<typeof SearchBar> = (args) => (
-  <SearchBar {...args} />
-)
+const Template: ComponentStory<typeof SearchBar> = () => <SearchBar />
 
 export const Default = Template.bind({})
 Default.args = {}
