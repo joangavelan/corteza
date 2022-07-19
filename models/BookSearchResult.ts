6b86d8dc2588892bc43aa80f13b4776680acf3a6
Joan Gavelán <joangavelan@gmail.com>
2022-07-18 19:42:02 -0500

    fix: remove unused files

--- models/BookSearchResult.ts
+++
@@ -1,6 +0,0 @@
-export interface BookSearchResultProps {
-  title: string;
-  author: string | undefined;
-  rating: number | undefined;
-  imgURL: string | undefined;
-}
--- models/index.ts
+++ models/index.ts
@@ -1,6 +1,5 @@
 export * from './Book.model';
 export * from './BookPreview.model';
-export * from './BookSearchResult';
 export * from './Button.model';
 export * from './Entry.model';
 export * from './Heading.model';
