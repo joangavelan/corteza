1e3989a0d6c824030db4c1efe9d740a974a8acf1
Joan Gavelán <joangavelan@gmail.com>
2022-07-25 19:29:31 -0500

    refactor: use arrow functions

--- hooks/useWindowDimensions.hook.ts
+++ hooks/useWindowDimensions.hook.ts
@@ -1,17 +1,19 @@
-import { useState, useEffect } from "react";
-import { getWindowDimensions } from "@utils";
+import { useState, useEffect } from 'react'
+import { getWindowDimensions } from '@utils'
 
-export function useWindowDimensions() {
-  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+export const useWindowDimensions = () => {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  )
 
   useEffect(() => {
-    function handleResize() {
-      setWindowDimensions(getWindowDimensions());
+    const handleResize = () => {
+      setWindowDimensions(getWindowDimensions())
     }
 
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
 
-  return windowDimensions;
+  return windowDimensions
 }
