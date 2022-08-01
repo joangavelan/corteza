ee18b2bb809f481f7c3abeb37b3d479f983fb15f
Joan Gavelán <joangavelan@gmail.com>
2022-08-01 15:33:09 -0500

    feat: create persistent global state for tracking books with zustand

---
+++ zustand/useBooks.ts
@@ -0,0 +1,40 @@
+import { Book, SettingsData } from '@models'
+import create from 'zustand'
+import { persist } from 'zustand/middleware'
+
+interface State {
+  books: Book[]
+  addBook: (book: Book) => void
+  deleteBook: (id: string) => void
+  updateBook: (id: string, data: SettingsData) => void
+}
+
+const useBooks = create<State>()(
+  persist(
+    (set) => ({
+      books: [],
+      addBook: (book) =>
+        set((state) => ({
+          books: [...state.books, book]
+        })),
+      deleteBook: (id) =>
+        set((state) => ({
+          books: state.books.filter((book) => book.id !== id)
+        })),
+      updateBook: (id, data) =>
+        set((state) => ({
+          books: state.books.map((book) => {
+            if (book.id === id) {
+              return { ...book, ...data }
+            }
+            return book
+          })
+        }))
+    }),
+    {
+      name: 'corteza-app'
+    }
+  )
+)
+
+export default useBooks
