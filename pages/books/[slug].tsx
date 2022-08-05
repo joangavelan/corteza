79581e9b1d13feea5b2c1fbdcecd9dc130616978
Joan Gavelán <joangavelan@gmail.com>
2022-08-05 11:36:57 -0500

    feat: implement book page sidebar

--- pages/books/[slug].tsx
+++ pages/books/[slug].tsx
@@ -1,12 +1,30 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 import { useRouter } from 'next/router'
 import useBooks from '@zustand/useBooks'
+import { useState } from 'react'
+import styles from '@styles/Book.module.scss'
+import CircularProgress from '@components/CircularProgress'
+import BookMeta from '@components/BookMeta'
+import { AiFillMinusCircle, AiFillPlusCircle } from 'react-icons/ai'
+import Settings from '@components/Settings'
+import Modal from '@components/Modal'
+import Button from '@components/Button'
+import useWarningMessage from '@zustand/useWarningMessage'
+import WarningMessage from '@components/WarningMessage'
 
 const Book: NextPage = () => {
+  const [pageControllerNumber, setPageControllerNumber] = useState(10)
+  const [openSettings, setOpenSettings] = useState(false)
+  const [openBookDeletionDialog, setOpenBookDeletionDialog] = useState(false)
   const router = useRouter()
   const { slug } = router.query
   const books = useBooks((state) => state.books)
   const book = books.find((book) => book.slug === slug)
+  const addReadPages = useBooks((state) => state.addReadPages)
+  const removeReadPages = useBooks((state) => state.removeReadPages)
+  const deleteBook = useBooks((state) => state.deleteBook)
+  const warningMessage = useWarningMessage((state) => state.message)
 
   if (!book) {
     return (
@@ -16,9 +34,131 @@ const Book: NextPage = () => {
     )
   }
 
+  const handlePageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target
+    if (!isNaN(Number(value))) {
+      setPageControllerNumber(Number(value))
+    }
+  }
+
+  const preventNonNumericInput = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    const allowedKeys = ['Backspace', 'ArrowLeft', 'ArrowRight']
+    if (isNaN(Number(e.key)) && !allowedKeys.includes(e.key)) {
+      e.preventDefault()
+    }
+  }
+
+  const handleAddReadPages = () => {
+    if (book.currentPage + pageControllerNumber <= book.pageCount) {
+      addReadPages(book.id, pageControllerNumber)
+    } else {
+      addReadPages(book.id, book.pageCount - book.currentPage)
+    }
+  }
+
+  const handleRemoveReadPages = () => {
+    if (book.currentPage - pageControllerNumber >= 0) {
+      removeReadPages(book.id, pageControllerNumber)
+    } else {
+      removeReadPages(book.id, book.currentPage)
+    }
+  }
+
+  const handleBookDeletion = () => {
+    deleteBook(book.id)
+    router.push('/')
+  }
+
   return (
-    <div>
-      <h1>{book?.title}</h1>
+    <div className={styles.container}>
+      <Head>
+        <title>{book.title}</title>
+        <meta name='description' content={`Tracking ${book.title}`} />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      {/* sidebar */}
+      <aside className={styles.sidebar}>
+        {/* header */}
+        <header className={styles.header}>
+          <div className={styles.logo} onClick={() => router.push('/')}>
+            Corteza<span> | Book Tracker</span>
+          </div>
+          <div className={styles.pageController}>
+            <AiFillMinusCircle onClick={handleRemoveReadPages} />
+            <input
+              type='text'
+              onKeyDown={preventNonNumericInput}
+              value={pageControllerNumber}
+              onChange={handlePageChange}
+            />
+            <AiFillPlusCircle onClick={handleAddReadPages} />
+          </div>
+        </header>
+        {/* book */}
+        <div>
+          {/* circular progress */}
+          <CircularProgress
+            title={book.title}
+            imgURL={book.imgURL}
+            currentPage={book.currentPage}
+            pageCount={book.pageCount}
+          />
+          {/* book meta */}
+          <BookMeta
+            title={book.title}
+            author={book.author}
+            rating={book.rating}
+            category={book.category}
+            pageCount={book.pageCount}
+            publishedYear={book.publishedYear}
+          />
+        </div>
+        {/* footer */}
+        <footer className={styles.footer}>
+          <a onClick={() => router.push('/')}>New Search</a>
+          <a onClick={() => setOpenSettings(true)}>Book Settings</a>
+          <a onClick={() => setOpenBookDeletionDialog(true)}>
+            Delete This Book
+          </a>
+        </footer>
+      </aside>
+      <main className={styles.main}></main>
+      {/* -- Dialogs-- */}
+      {/* settings */}
+      {openSettings && (
+        <Modal setOpen={setOpenSettings}>
+          <Settings
+            selectedBook={book}
+            setOpenSettings={setOpenSettings}
+            title='Book Settings'
+          />
+        </Modal>
+      )}
+      {/* book deletion */}
+      {openBookDeletionDialog && (
+        <Modal setOpen={setOpenBookDeletionDialog}>
+          <div className={styles.bookDelitionDialog}>
+            <h3>Are you sure you want to delete this book?</h3>
+            <p>All of the data related to this book will be lost.</p>
+            <Button
+              size='medium'
+              color='dark'
+              type='button'
+              onClick={() => setOpenBookDeletionDialog(false)}
+              text='Cancel'
+            />
+            <Button
+              size='medium'
+              color='light'
+              type='button'
+              onClick={handleBookDeletion}
+              text='Delete'
+            />
+          </div>
+        </Modal>
+      )}
+      {/* feedback message */}
+      {warningMessage && <WarningMessage />}
     </div>
   )
 }
